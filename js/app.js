8e10717a163fb7d228ddb0ab03d43696cd86d7e7
yuliia <yuliia.dorofeieva@gmail.com>
2020-05-16 15:59:53 +0200

    Make sections collapsible, setHeightOfContent

--- js/app.js
+++ js/app.js
@@ -27,15 +27,10 @@ const navList = document.querySelector('#navbar__list');
 const header_nav = document.querySelector(".page__header");
 
 
-
-
 // Detect Viewport
 
 
-var sec1 = document.querySelector('#section1');
-var sec2 = document.querySelector('#section2');
-
-
+const btnCollapse = document.querySelectorAll(".btn-collapse")
 
 
 
@@ -48,6 +43,15 @@ var sec2 = document.querySelector('#section2');
  * 
 */
 
+// Set the absolute value for the height property to animate collapsable sections ( "transition" works when the property changes)
+const setHeightOfContent = () => {
+    const sectionsContent = document.querySelectorAll(".content");
+    for (content of sectionsContent) {
+        content.style.height = content.scrollHeight + "px";
+        // console.log('Set Height of content:', content.parentNode.parentNode.getAttribute("id"));
+    }
+    console.log('Set Height of content');
+}
 // detect if element is in the viewport/closest to the top
 const isInViewport = function (elem) {
     let bounding = elem.getBoundingClientRect();
@@ -55,7 +59,7 @@ const isInViewport = function (elem) {
     // if (bounding.top < window.innerHeight && bounding.bottom > 0)
     return (
         bounding.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-        (bounding.top >= 0 || bounding.bottom > (window.innerHeight/2) )
+        (bounding.top >= 0 || bounding.bottom > (window.innerHeight/2 || document.documentElement.clientHeight/2) )
     );
 };
 
@@ -65,6 +69,7 @@ const removeCurrentActiveClass = (elem) => {
         currentActive[0].classList.remove("your-active-class");
           }
 }
+
 
 /**
  * End Helper Functions
@@ -77,9 +82,6 @@ const buildNavMenu = () => {
     //  loop to detect all sections and compile into #navbar__list
     for (const section of allsections) {
         const navLi = document.createElement('li');
-        // const nav_a = document.createElement('a');
-        // nav_a.setAttribute('href', '#'+sect_id);
-        // navLi.appendChild(nav_a);
         const sect_id = section.getAttribute('id');
         const sect_title = section.getAttribute('data-nav');
         navLi.setAttribute('id', 'nav-'+sect_id);
@@ -87,19 +89,37 @@ const buildNavMenu = () => {
         navLi.textContent = sect_title;
         navList.appendChild(navLi);
     }
-     // console.log('buildNavMenu func loaded');
+     console.log('buildNavMenu func loaded');
 }
-
+// Hide fixed navigation bar while not scrolling (it should still be present on page load).
+// Hint: setTimeout can be used to check when the user is no longer scrolling.
+const hideNavMenu = () => {
+    setTimeout(function() { 
+        header_nav.style.visibility = "hidden";
+        // alert("Hello"); 
+    }, 4000);
+};
+const noHideNavMenu = () => {
+    header_nav.style.visibility = "visible";
+    // navList.style.display = "block";
+};
+const appearNavMenu = () => {
+    noHideNavMenu();
+    hideNavMenu();
+    // if(navList.mouseIsOver) {
+    //     navList.style.display = "block";
+    // }
+};
 
 // Add class 'active' to section when near top of viewport 
 
 const setasActive = () => {
-
+    console.log('setasActive');
     for (const section_ of allsections) {
         
         if (isInViewport(section_)) {
             const nav_of_section = navList.querySelector('#nav-'+section_.id);
-
+            // console.log('setasActive:', section_.getAttribute('id'));
             // remove previous active class 
             removeCurrentActiveClass(nav_of_section);
             removeCurrentActiveClass(section_);
@@ -118,30 +138,25 @@ function scrollTO(evt) {
     const idToScroll = evt.target.getAttribute('data-anchor');
     const elmnt = document.getElementById(idToScroll);
     elmnt.scrollIntoView({behavior: "smooth"});
-    // console.log('scrollTO func loaded');
+    console.log('scrollTO func loaded');
 }
 
-// Hide fixed navigation bar while not scrolling (it should still be present on page load).
-// Hint: setTimeout can be used to check when the user is no longer scrolling.
-const hideNavMenu = () => {
-    setTimeout(function() { 
-        header_nav.style.visibility = "hidden";
-        // alert("Hello"); 
-    }, 4000);
-}
-const noHideNavMenu = () => {
-    header_nav.style.visibility = "visible";
-    // navList.style.display = "block";
-}
-const appearNavMenu = () => {
-    noHideNavMenu();
-    hideNavMenu();
-    // if(navList.mouseIsOver) {
-    //     navList.style.display = "block";
-    // }
-}
+const collapseSection = (e) => {
+    const content = e.target.nextElementSibling;
+  
+    if (e.target && e.target.matches("button.btn-collapse")) {
+        e.target.classList.toggle("active");
 
-
+        if (content.isCollapsed) {
+            content.style.height = content.scrollHeight + "px";
+            content.isCollapsed = false;
+        } else {
+            content.style.height = "0px";
+            content.isCollapsed = true;
+        }
+    }
+  
+}
 
 /**
  * End Main Functions
@@ -149,19 +164,24 @@ const appearNavMenu = () => {
  * 
 */
 
-document.addEventListener("DOMContentLoaded", function() {
-    
+
+
+window.addEventListener("load", function() {
     // Build menu 
     buildNavMenu();
 
+    // set absolute height of collapsable sections of content to make slow transition
+    setHeightOfContent();
+
     // Scroll to section on link click
     navList.addEventListener("click", scrollTO);
+
+    //  Make sections collapsible
+    document.addEventListener("click", collapseSection);
 
  
     // Set sections as active
     window.addEventListener('scroll', setasActive);
     window.addEventListener('scroll', appearNavMenu);
     window.addEventListener('mouseover', noHideNavMenu);
-
-
 });
